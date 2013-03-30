240a4e366987ac5d9c07bd1026b0f1653ca44d9d
devinmcinnis <devin.mcinnis@gmail.com>
2013-03-29 21:21:12 -0700

    setup cron job; properly pulling info from easportsworld

--- app.js
+++ app.js
@@ -7,7 +7,37 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , request = require('request')
+  , jsdom = require('jsdom')
+  , routes = require('./routes')
+  , pg = require('pg')
+  , connectionString = process.env.DATABASE_URL || 'postgres://eashl:eashl@localhost/template1'
+  , client
+  , query;
+
+client = new pg.Client(connectionString);
+client.connect();
+
+// Queries are queued and executed one after another once the connection becomes available
+client.query("CREATE TEMP TABLE team(team_id varchar, name varchar)");
+client.query("CREATE TEMP TABLE game(game_id integer, team1_id varchar, team1_score integer, team2_id varchar, team2_score integer)");
+client.query("CREATE TEMP TABLE player(player_id integer, name varchar)");
+client.query("CREATE TEMP TABLE stats(player_id integer, game_id integer, goals integer, assists integer, points integer, plus_minus integer, pim integer, ppg integer, shg integer, total_hits integer, bs integer, shots integer, shooting_p integer, gaa integer, saves integer, save_p integer, so integer)");
+client.query("INSERT INTO team(team_id, name) values($1, $2)", ['224', 'Puck Goes First']);
+var query = client.query("SELECT * FROM team WHERE name = $1", ['Puck Goes First']);
+
+// Can stream row results back 1 at a time
+query.on('row', function(row) {
+  console.log(row);
+  console.log("Team name is " + row.name);
+  console.log("Team ID is " + row.team_id);
+});
+
+// Fired after last row is emitted
+query.on('end', function() { 
+  client.end();
+});
 
 var app = express();
 
@@ -28,8 +58,56 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+var currentRecord = ['78', '50', '14'];
+var bloop = new Date();
+// bloop.setSeconds(bloop.getSeconds() + 5);
+
+var cronJob = require('cron').CronJob;
+new cronJob(bloop, function(){
+
+  if ( typeof currentRecord != 'undefined' ) {
+    console.log('Checking to see if team has played a game..');
+    console.log('Team\'s current record is: ' + currentRecord.join('-')); }
+
+  var teamRecord = 'http://www.easportsworld.com/en_US/clubs/401A0001/224/overview';
+
+  request({uri: teamRecord}, function (err, response, body) {
+    // Basic error check
+    if ( err && response.statusCode != 200 ) {console.log('Request error.')}
+    // Send the body parameter as the HTML code we will parse in jsdom
+    // Also, tell jsdom to attach jQuery in the scripts
+    jsdom.env({
+      html: body,
+      scripts: ['http://code.jquery.com/jquery-1.6.min.js']
+    },
+    function (err, window) {
+      var $ = window.jQuery,
+      $body = $('body .current-season-club-stats-main-container'),
+      record = $body.find('tr.strong > td:nth-child(2) span.black').text().split(' - ');
+
+      // For each part of record (wins-losses-ties), check against teams current record and get the game stats from the most recently played game if it's different
+      record.map(function (callback, key) {
+
+        if (typeof currentRecord === 'undefined') {
+          console.log('Created team record of: ' + record.join('-'));
+        } else if ( record[key] != currentRecord[key] && currentRecord.length > 2 ) {
+          console.log('Team\'s new record is: ' + record.join('-'));
+          routes.getLatestGame();
+        } else {
+          return;
+        }
+
+      });
+
+      return currentRecord = record;
+
+    });
+  });
+}, null, true);
+
+app.get('/', function(req, res) {
+
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,205 @@
+var stats  = 'http://www.easportsworld.com/en_US/clubs/partial/401A0001/224/members-list',
+    games  = 'http://www.easportsworld.com/en_US/clubs/partial/401A0001/224/match-results?type=all',
+    game_id = '';
 
-/*
- * GET home page.
- */
+var request = require('request'),
+    jsdom = require('jsdom');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'EASHL PGF Stat Logger' });
 };
+
+exports.getLatestGame = function (req, res) {
+
+  var self = this,
+      game_players = [];
+      self.team = {};
+
+  console.log('Searching easportsworld.com for the ID of the most recent game played...');
+  console.log('This may take a minute or so.');
+  
+  request({uri: games}, function (err, response, body) {
+    // Basic error check
+    if ( err && response.statusCode != 200 ) {console.log('Request error.')}
+    // Send the body parameter as the HTML code we will parse in jsdom
+    // Also, tell jsdom to attach jQuery in the scripts and loaded from jQuery.com
+    jsdom.env({
+      html: body,
+      scripts: ['http://code.jquery.com/jquery-1.6.min.js']
+    },
+    function (err, window) {
+    
+      var $ = window.jQuery,
+      $body = $('body #widgets > table > tbody > tr:first-child');
+      gameId = $body.find('.match-details-button').attr('rel');
+
+      self.team.gameId = gameId;
+
+      return getPlayersOfLastGame();
+
+    });
+  });
+
+  function getPlayersOfLastGame() {
+    var game = 'http://www.easportsworld.com/en_US/clubs/partial/401A0001/224/match-results/details?match_id='+ self.team.gameId + '&type=all';
+
+    console.log('Getting a list of the players that played in the last game...');
+
+    // Tell the request that we want to fetch eashl.com, send the restuls to a function
+    request({uri: game}, function (err, response, body) {
+      // Basic error check
+      if ( err && response.statusCode != 200 ) {console.log('Request error.')}
+      // Send the body parameter as the HTML code we will parse in jsdom
+      // Also, tell jsdom to attach jQuery in the scripts and loaded from jQuery.com
+      jsdom.env({
+        html: body,
+        scripts: ['http://code.jquery.com/jquery-1.6.min.js']
+      },
+      function (err, window) {
+        // Use jQuery just as in regular HTML
+        var $ = window.jQuery,
+        $body = $('body'),
+        players = $body.find('.yui-u.first tr > th > div > a');
+        // console.log(players);
+        players.each(function() {
+          game_players.push($(this).text());
+        });
+
+        return getPlayerStatsOfLastGame();
+      });
+    });
+  }
+
+  function getPlayerStatsOfLastGame() {
+
+    console.log('Getting the stats of the players in the last game...');
+
+    // Tell the request that we want to fetch eashl.com, send the restuls to a function
+    request({uri: stats}, function (err, response, body) {
+      // Basic error check
+      if ( err && response.statusCode != 200 ) {console.log('Request error.')}
+      // Send the body parameter as the HTML code we will parse in jsdom
+      // Also, tell jsdom to attach jQuery in the scripts and loaded from jQuery.com
+      jsdom.env({
+        html: body,
+        scripts: ['http://code.jquery.com/jquery-1.6.min.js']
+      },
+      function (err, window) {
+        // Use jQuery just as in regular HTML
+        var $ = window.jQuery,
+        $body = $('body'),
+        $player = $body.find('tbody tr'),
+        $category = $player.find('td');
+
+        $player.each(function (i, row) {
+          var $row = $(row);
+          var playername = $(row).find('td:nth-child(2) a').text();
+         
+          game_players.map(function (callback, key) {
+            if (game_players[key] == playername) {
+              self.team[playername] = {};
+
+              $category.each(function (i, stat) {
+                var statname = $row.find(stat).attr('title');
+                var $stat = $row.find(stat);
+                if ( $stat.index() > 2 ) {
+                  self.team[playername][statname] = $stat.text();
+                }
+              });
+            }
+          });
+        });
+
+        getGameInfoOfLastGame();
+      });
+    });
+  }
+
+  function getGameInfoOfLastGame() {
+
+    console.log('Getting game information of the most recent game played...');
+
+    request({uri: games}, function (err, response, body) {
+      // Basic error check
+      if ( err && response.statusCode != 200 ) {console.log('Request error.')}
+      // Send the body parameter as the HTML code we will parse in jsdom
+      // Also, tell jsdom to attach jQuery in the scripts and loaded from jQuery.com
+      jsdom.env({
+        html: body,
+        scripts: ['http://code.jquery.com/jquery-1.6.min.js']
+      },
+      function (err, window) {
+      
+        var $ = window.jQuery,
+        $body = $('body #widgets > table > tbody > tr:first-child'),
+        $opp = $body.find('.align-right.team'),
+        game_score = $body.find('match-result-score').text().split('-'),
+        date = $body.find('.score .strong').text().split(' '),
+        time = date[1].split(':'),
+        hours = (date[2] === 'PM') ? time[0] : time[0] + 12;
+
+        self.team.date = new Date();
+        self.team.date.setHours(hours);
+        self.team.date.setMinutes(time[1]);
+        self.team.date.setSeconds('00');
+
+        var reg = /^[a-z0-9]+$/gmi;
+        self.team.opp = {
+          name: $opp.find('a').text(),
+          url: $opp.find('a').attr('href')
+        }
+        $opp.find('a').remove();
+        self.team.opp['abbr'] = $opp.find('div').text().replace(/\W/g, '');
+
+        logResults();
+      });
+    }); 
+  }
+
+  function logResults() {
+    console.log('Results of the last game played:')
+    console.log(self.team);
+  }
+
+};
+
+exports.fillStats = function (req, res) {
+
+  var self = this;
+      self.team = {};
+
+  request({uri: stats}, function (err, response, body) {
+    // Basic error check
+    if ( err && response.statusCode != 200 ) {console.log('Request error.')}
+    // Send the body parameter as the HTML code we will parse in jsdom
+    // Also, tell jsdom to attach jQuery in the scripts and loaded from jQuery.com
+    jsdom.env({
+      html: body,
+      scripts: ['http://code.jquery.com/jquery-1.6.min.js']
+    },
+    function (err, window) {
+      // Use jQuery just as in regular HTML
+      var $ = window.jQuery,
+      $body = $('body'),
+      $player = $body.find('tbody tr'),
+      $category = $player.find('td');
+
+      $player.each(function (i, row) {
+        var $row = $(row);
+        var playername = $(row).find('td:nth-child(2) a').text();
+
+        self.team[playername] = {};
+
+        $category.each(function (i, stat) {
+          var statname = $row.find(stat).attr('title');
+          var $stat = $row.find(stat);
+          if ( $stat.index() > 2 ) {
+            self.team[playername][statname] = $stat.text();
+          }
+        });
+      });
+
+      res.end();
+    });
+  });
+}
